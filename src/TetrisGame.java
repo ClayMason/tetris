e4306ac789ae4ff51c375cf7828a6569e155264d
Muiz Yusuff <muiz.project@gmail.com>
2019-06-29 16:40:54 -0400

    Added shapes

--- src/TetrisGame.java
+++ src/TetrisGame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TetrisGame extends JPanel {
@@ -10,8 +11,13 @@ public class TetrisGame extends JPanel {
     Image background;
     Image[] blocks;
     int[][] grid;
+    JFrame frame;
+    Random rand_;
+    ArrayList<int[][]> shapes;
 
     public TetrisGame () {
+
+        rand_ = new Random ();
 
         this.setup ();
         System.out.print (background.getWidth(null) + ", " + background.getHeight(null));
@@ -49,18 +55,65 @@ public class TetrisGame extends JPanel {
             }
         }
 
+        /* setup the shapes
+        *
+        *  0: X X
+        *     X X
+        *
+        *  1: X X X X
+        *
+        *  2: X
+        *     X
+        *     X X
+        *
+        *  3: X
+        *     X X
+        *       X
+        *
+        *  4:   X
+        *     X X X
+        *
+        */
+        int[][] shape_0 = new int[][] {{1, 1},{1, 1}};
+        int[][] shape_1 = new int[][] {{1, 1, 1, 1}};
+        int[][] shape_2 = new int[][] {{1, 0},{1, 0},{1, 1}};
+        int[][] shape_3 = new int[][] {{1, 0},{1, 1},{0, 1}};
+        int[][] shape_4 = new int[][] {{0, 1, 0},{1, 1, 1}};
+
+        shapes = new ArrayList<int[][]> ();
+        shapes.add(shape_0);
+        shapes.add(shape_1);
+        shapes.add(shape_2);
+        shapes.add(shape_3);
+        shapes.add(shape_4);
+        
         // set frame
-        JFrame frame = new JFrame("Tetris Game");
+        frame = new JFrame("Tetris Game");
         frame.add(this);
-        frame.setSize(450,  639);
-        frame.setVisible(true);
+        frame.setSize(450,  689);
+        // frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 
     public void start (){// throws java.io.IOException {
 
         // start the tetris game
         System.out.print("Game started\n");
+        EventQueue.invokeLater(() -> {
+            frame.setVisible(true);
+        });
+
+        while ( true ) {
+            repaint ();
+            try {
+
+                Thread.sleep ( 100 );
+            }
+            catch (java.lang.InterruptedException e) {
+                System.out.print ("interrupted.\n");
+            }
+        }
 
     }
 
@@ -78,10 +131,12 @@ public class TetrisGame extends JPanel {
             for ( int j = 0; j < grid[0].length; ++j ) {
 
                 int x_pos = 100 + (j * 25);
-                int y_pos = 100 + (i * 25);
+                int y_pos = 150 + (i * 25);
 
                 Image block = blocks[grid[i][j] - 1];
                 g2d.drawImage(block, x_pos, y_pos, 25, 25, null);
+
+                grid[i][j] = rand_.nextInt(6)+1;
 
             }
         }
